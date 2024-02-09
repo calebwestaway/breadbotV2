467cbfd99ede08bc2d11b3c72b647f22d9b525cd
Caleb Westaway <calebwestaway@icloud.com>
2024-02-09 21:08:27 +0000

    Actually fix setup.sh (I promise it works this time)

--- setup.sh
+++ setup.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
 
-cd "$(git rev-parse --show-toplevel)"
-
-read -p "Run setup script? You must have Node v16.11.0 or higher: " -n 1 -r
+read -p "Run setup script? You must have Node installed: " -n 1 -r
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
     cd "$(git rev-parse --show-toplevel)"
 
-    echo "{
-    }" > temp.json
-    echo "Created temp.json"
+    if ! [ -f temp.json ]; then
+        echo "{}" > temp.json
+        echo "Created temp.json"
+    else
+        echo "Found temp.json, skipping"
+    fi
 
-    echo "
-    {
-	    "token": "insert token here",
-	    "clientId": "inset client id here"
-    }
-    " > config.json
-    echo "Created config.json"
+    if ! [ -f config.json ]; then
+        echo '
+{
+    "token": "insert token here",
+    "clientId": "insert client id here"
+}
+        ' > config.json
+        echo "Created config.json"
+    else
+        echo "Found config.json, skipping"
+    fi
 
-    touch channels.json
-    echo "Created channel.json."
+    if ! [ -f channels.json ]; then
+        touch channels.json
+        echo "Created channels.json."
+    else
+        echo "Found channels.json, skipping"
+    fi
+    
+    echo "Installing required packages via npm"
+    npm install
+    wait
 
     echo "Running channelScanner.js"
     node functions/channelScanner.js
-
     wait
 
-    echo "Installing required packages"
-    npm install
-
-    wait
 
     echo "Setup complete!"
     echo "Please fill in config.json with the correct data"
 
-elif [[ $REPLY =~ ^[Nn]$ ]]
-then
+elif [[ $REPLY =~ ^[Nn]$ ]]; then
     echo "Not running setup script"
 fi
